762760de552add5f717f23edf3b52ccda69415cb
Jabby Alicante <alicantejabbylumenso@gmail.com>
2024-08-30 21:33:21 +0800

    Project

--- hambmenu.js
+++ hambmenu.js
@@ -2,3 +2,13 @@
 document.getElementById('hamb-menu').addEventListener('click', function(){
     document.getElementById('menu').classList.toggle('show');
 });
+
+function playVideo(){
+    const video= document.getElementById('media-video')
+    const image= document.querySelector('media-image')
+
+    image.style.opacity=0;
+    video.style.opacity=1;
+    video.style.pointerEvents= 'auto';
+    video.play();
+}
