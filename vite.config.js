1a496a5877fa34985ba4f70676efc311b7fe3ab4
Beatrice Dela Cruz <delacruzbeatrice10@gmail.com>
2026-08-13 19:10:00 +0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beatricecoleene.github.io/',
+  base: '/',
 
   server:{
     host: '0.0.0.0',
