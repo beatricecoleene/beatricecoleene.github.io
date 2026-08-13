328c5aea7df7c62e2c565fb8060fa0118061179f
Beatrice Dela Cruz <delacruzbeatrice10@gmail.com>
2026-08-13 18:48:23 +0800

    fixes gh and base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/beatricecoleene.github.io/',
+
   server:{
     host: '0.0.0.0',
     allowedHosts: true
